aaa01634b8d4d3970f78e7a7cd7f311f3d63137e
theharishpuri <theharishpuri@gmail.com>
2026-02-12 15:54:58 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,26 @@
+const CACHE_NAME = "snake-v1";
+
+const FILES_TO_CACHE = [
+  "/",
+  "index.html",
+  "style.css",
+  "script.js",
+  "manifest.json"
+];
+
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(res => {
+      return res || fetch(e.request);
+    })
+  );
+});
 
